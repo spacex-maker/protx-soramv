08e0d14e8410c0cfb07cab561986bcb0167a6d10
spacex-maker <wzdhfn@gmail.com>
2026-06-25 23:45:31 +0800

    新增人机验证系统

--- src/pages/Login/components/RightSection/index.js
+++ src/pages/Login/components/RightSection/index.js
@@ -14,6 +14,7 @@ import {
   MailOutlined,
   LockOutlined,
 } from '@ant-design/icons';
+import GoogleGIcon from './GoogleGIcon';
 import CaptchaField from 'components/security/CaptchaField';
 import { 
   SiWechat, 
